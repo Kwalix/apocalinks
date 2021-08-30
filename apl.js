33a60793b973ecdae78620226a7ccbce20115c53
Kiwi <hallerlucas@outlook.com>
2021-08-30 21:56:21 +0200

    added list command

--- apl.js
+++ apl.js
@@ -2,13 +2,13 @@ const yargs = require('yargs');
 const path = require('path');
 const chalk = require('chalk');
 const baseUrl = require('./baseUrl');
-const { appendFileSync } = require('fs');
+const { appendFileSync, readFileSync } = require('fs');
 const emojis = require('node-emoji');
 const clipboardy = require('clipboardy');
 const validator = require('validator');
 
 
-yargs.scriptName("apl");
+ /* yargs.scriptName("apl"); */
 
 const txtFilePath = path.join(__dirname, 'links.txt');
 
@@ -37,13 +37,25 @@ yargs.command('add', 'page to store, ex: statistic-2020', {
 }, argv => {
     const { page, source, medium, campaign} = argv;
     const link = `${baseUrl}${page}?utm_source=${source}&utm_medium=${medium}&utm_campaign=${campaign}`;
-    
-    if (!validator.isURL(link)) {
-        console.log(chalk.redBright('Source URL is not valid, please check and retry'));
-        return;
+
+    try {
+        if (!validator.isURL(link)) {
+            throw new Error('Source URL is not valid, please check and retry');
+        }
+    } catch (error) {
+        console.log(chalk.redBright(error.message));
     }
 
-    appendFileSync(txtFilePath, link + '\n');
+    appendFileSync(txtFilePath, `${link}\n`);
     clipboardy.writeSync(link);
+
     console.log(chalk.green(emojis.emojify('Link saved and copied to clipboard ! :chains: :point_down: \n' + link)));
+}).argv;
+
+
+yargs.command('list', 'list all stored links', (argv) => {
+    const links = [...readFileSync(txtFilePath).toString().split(/\n/)];
+    for (const i in links) {
+        console.log(`${emojis.emojify(':star:')}: ${links[i]}`);
+    }
 }).argv;
